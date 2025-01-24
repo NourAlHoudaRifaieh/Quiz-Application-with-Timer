197d04895cbebb300c9b8342df14fa39b69a9310
NourAlHoudaRifaieh <nouralhoudarifaieh@gmail.com>
2025-01-25 00:10:08 +0200

    edits

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//selecting all required element s
+//selecting all required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
